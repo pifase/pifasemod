bea373fcdfdaa7a318ed4ee97c2ea10f871c688a
pifase <pifasedoesminecraft@gmail.com>
2021-05-13 15:38:06 -0400

    Added Pifase mob, Added Octoturt Mob, Added Pifase Block, Added Pifase Dimension

---
+++ src/main/java/net/mcreator/pifase/block/PifaseBlockBlock.java
@@ -0,0 +1,49 @@
+
+package net.mcreator.pifase.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.pifase.PifaseModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@PifaseModElements.ModElement.Tag
+public class PifaseBlockBlock extends PifaseModElements.ModElement {
+	@ObjectHolder("pifase:pifase_block")
+	public static final Block block = null;
+	public PifaseBlockBlock(PifaseModElements instance) {
+		super(instance, 3);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(1.3f, 10f).setLightLevel(s -> 0));
+			setRegistryName("pifase_block");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
